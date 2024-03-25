4e8eee96662d6c00c5b0cac974f6ab97967ec1dd
ceedadev <ceedadev@gmail.com>
2024-03-25 20:04:21 +0700

    Add todos orderBy createdAt and implement CRUD operations for tenants

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { Elysia } from "elysia";
+import { desc, eq, sql } from "drizzle-orm";
 
 import { db } from "./db";
-import { todos } from "./db/schema";
+import { tenants, todos } from "./db/schema";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .get("/todos", () => {
-    const todos = db.query.todos.findMany();
+    const todos = db.query.todos.findMany({
+      orderBy: (t, { desc }) => [desc(t.createdAt)],
+    });
     return todos;
   })
   .post("/todos", (req) => {
@@ -19,6 +22,40 @@ const app = new Elysia()
       description: description,
     });
     return insertTodo.returning();
+  })
+  .put("/todos/:id", ({ params: { id }, body }) => {
+    const { title, description } = body as {
+      title: string;
+      description: string;
+    };
+    const updateTodo = db
+      .update(todos)
+      .set({
+        title: title,
+        description: description,
+        updatedAt: sql`now()`,
+      })
+      .where(eq(todos.id, Number(id)));
+    return updateTodo.returning();
+  })
+  .get("/tenants", () => {
+    const tenants = db.query.tenants.findMany();
+    return tenants;
+  })
+  .post("/tenants", (req) => {
+    const { name } = req.body as {
+      name: string;
+    };
+    const insertTenant = db.insert(tenants).values({
+      name: name,
+    });
+    return insertTenant.returning();
+  })
+  .get("/tenants/:id", ({ params: { id } }) => {
+    const tenant = db.query.tenants.findFirst({
+      where: (t) => eq(t.id, id),
+    });
+    return tenant;
   })
   .listen(3000);
 
