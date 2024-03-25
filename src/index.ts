f5bd406265fbc605f890339d8047f594be61586b
ceedadev <ceedadev@gmail.com>
2024-03-25 17:00:18 +0700

    Add CRUD operations for todos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { db } from "./db";
+import { todos } from "./db/schema";
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/todos", () => {
+    const todos = db.query.todos.findMany();
+    return todos;
+  })
+  .post("/todos", (req) => {
+    const { title, description } = req.body as {
+      title: string;
+      description: string;
+    };
+    const insertTodo = db.insert(todos).values({
+      title: title,
+      description: description,
+    });
+    return insertTodo.returning();
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
